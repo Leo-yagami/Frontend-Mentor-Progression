7d18bc53d88da90c673f1895f67145064c75aefe
Haitham Abdulhamid <118968574+Leo-yagami@users.noreply.github.com>
2024-08-16 23:41:03 +0300

    hero section(mobile)
    finished the main styling for the hero section of the website(mobile-first)

--- Challenges/Challenge6(clipboard)/clipboard(landing_page)/tailwind.config.js
+++ Challenges/Challenge6(clipboard)/clipboard(landing_page)/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkGrayBlue: "hsl(210, 10%, 33%)",
+        grayBlue: "hsl(201, 11%, 66%)",
+        strongCyan: "hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+      },
+    },
   },
   plugins: [],
 };
