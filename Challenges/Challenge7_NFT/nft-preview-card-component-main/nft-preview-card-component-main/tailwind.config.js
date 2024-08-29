424c5cf9845aacf27f97b62ae74b4a1fd4cd3013
Haitham Abdulhamid <118968574+Leo-yagami@users.noreply.github.com>
2024-08-29 22:46:49 +0300

    setup

---
+++ Challenges/Challenge7_NFT/nft-preview-card-component-main/nft-preview-card-component-main/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,css}"],
+  theme: {
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        heavyDarkBlue: "hsl(217, 54%, 11%)",
+        darkBlue: "hsl(216, 50%, 16%)",
+        heavyDarkBlue: "hsl(215, 32%, 27%)",
+        softBlue: "hsl(215, 51%, 70%)",
+        Cyan: "hsl(178, 100%, 50%)",
+      },
+    },
+  },
+  plugins: [],
+};
